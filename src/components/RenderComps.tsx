93a79168c3ed133b06dc91095634aac0f4e2af03
ashakirzianov <ashakirzianov@icloud.com>
2017-12-12 18:46:51 -0800

    Reimplemented Grid component

--- src/components/RenderComps.tsx
+++ src/components/RenderComps.tsx
@@ -7,25 +7,25 @@ export type Color = string;
 export const Line: SFC<{}> = props =>
     <div style={{
         display: "block",
-        marginBottom: -4, // TODO: find out better way
+        // marginBottom: -4, // TODO: find out better way
     }}>{props.children}</div>;
 export const Stack: SFC<{}> = props =>
     <div style={{ display: "inline-block" }}>{props.children}</div>;
 
-export const GridCell: SFC<{
-        color: string,
-        size: number,
-        borderRadius?: number,
-        margin?: number,
-    }> = props =>
-        <div style={{
-            background: props.color,
-            width: props.size,
-            height: props.size,
-            borderRadius: props.borderRadius,
-            margin: props.margin,
-            display: "inline-block",
-        }}/>;
+// export const GridCell: SFC<{
+//     color: string,
+//     size: number,
+//     borderRadius?: number,
+//     margin?: number,
+// }> = props =>
+//         <div style={{
+//             background: props.color,
+//             width: props.size,
+//             height: props.size,
+//             borderRadius: props.borderRadius,
+//             margin: props.margin,
+//             display: "inline-block",
+//         }} />;
 
 export type GridCellType = { color: Color };
 export const Grid: SFC<{
@@ -34,15 +34,15 @@ export const Grid: SFC<{
     borderRadius?: number,
     margin?: number,
 }> = props =>
-    <Stack>{
-        props.rows.map((r, i) =>
-        <Line key={i}>
-            { r.map((c, j) => <GridCell
-                key={j}
-                color={c.color}
-                size={props.cellSize}
-                borderRadius={props.borderRadius}
-                margin={props.margin}
-                />) }
-        </Line>)
-    }</Stack>;
+        <table style={{ display: "inline-block", borderSpacing: props.margin }}>
+            {props.rows.map(row =>
+                <tr>{row.map(col =>
+                    <td style={{
+                        background: col.color,
+                        width: props.cellSize,
+                        height: props.cellSize,
+                        borderRadius: props.borderRadius,
+                    }} />)}
+                </tr>)}
+        </table>
+    ;
