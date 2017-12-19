88a6a57d15e13d65ba6878b9137686dc53f4b209
ashakirzianov <ashakirzianov@icloud.com>
2017-12-18 17:15:23 -0800

    Minor renaming

--- src/model/logic.spec.ts
+++ src/model/logic.spec.ts
@@ -2,9 +2,9 @@ import { expect } from "chai";
 import { makeFigureLayer, removeFilled, needToRemove, indexesToRemove } from "./logic";
 
 describe("Logic", () => {
-    const ec = { cell: "empty" as "empty" };
-    const zc = { cell: "full" as "full", color: 0 };
-    const oc = { cell: "full" as "full", color: 1 };
+    const o = { cell: "empty" as "empty" };
+    const x = { cell: "full" as "full", color: 0 };
+    const y = { cell: "full" as "full", color: 1 };
     it("makeFigureLayer", () => {
         expect(makeFigureLayer({
             color: 0,
@@ -16,42 +16,42 @@ describe("Logic", () => {
             { rows: 5, cols: 5 },
             [2, 1]),
         ).to.be.deep.eq([
-            [ec, ec, ec, ec, ec],
-            [ec, ec, ec, ec, ec],
-            [ec, zc, zc, ec, ec],
-            [ec, zc, ec, ec, ec],
-            [ec, ec, ec, ec, ec],
+            [o, o, o, o, o],
+            [o, o, o, o, o],
+            [o, x, x, o, o],
+            [o, x, o, o, o],
+            [o, o, o, o, o],
         ]);
     });
 
     it("removeFilled", () => {
         expect(removeFilled([
-            [ec, zc, zc, ec, ec],
-            [ec, zc, zc, ec, ec],
-            [oc, zc, oc, zc, zc],
-            [ec, zc, zc, oc, oc],
-            [ec, zc, zc, ec, ec],
+            [o, x, x, o, o],
+            [o, x, x, o, o],
+            [y, x, y, x, x],
+            [o, x, x, y, y],
+            [o, x, x, o, o],
         ])).to.be.deep.eq([
-            [ec, ec, ec, ec, ec],
-            [ec, ec, ec, ec, ec],
-            [ec, ec, ec, ec, ec],
-            [ec, ec, ec, oc, oc],
-            [ec, ec, ec, ec, ec],
+            [o, o, o, o, o],
+            [o, o, o, o, o],
+            [o, o, o, o, o],
+            [o, o, o, y, y],
+            [o, o, o, o, o],
         ]);
     });
 
     it("needToRemove", () => {
-        expect(needToRemove([zc, zc, oc, oc, zc])).eq(true);
-        expect(needToRemove([zc, zc, ec, oc, zc])).eq(false);
+        expect(needToRemove([x, x, y, y, x])).eq(true);
+        expect(needToRemove([x, x, o, y, x])).eq(false);
     });
 
     it("indexesToRemove", () => {
         expect(indexesToRemove([
-            [ec, zc, zc, ec, ec],
-            [ec, zc, zc, ec, ec],
-            [oc, zc, oc, zc, zc],
-            [ec, zc, zc, oc, oc],
-            [ec, zc, zc, ec, ec],
+            [o, x, x, o, o],
+            [o, x, x, o, o],
+            [y, x, y, x, x],
+            [o, x, x, y, y],
+            [o, x, x, o, o],
         ])).deep.eq([2]);
     });
 });
