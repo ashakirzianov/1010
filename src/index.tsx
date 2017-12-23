41fb8c6e4d89640201bb277b41ec6f868ff56033
Anton Shakirzianov <ashakirzianov@icloud.com>
2017-12-23 02:04:45 -0800

    Added react-dnd with HTML5 backend

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { DragDropContext } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
 
 import { store } from "./redux/store";
 import { GameComp } from "./components/GameComps";
@@ -9,7 +11,7 @@ import { actionCreators } from "./redux/redux-utils";
 import { actionsTemplate } from "./model/actions";
 
 const allActionCreators = actionCreators(actionsTemplate);
-export const App = connectTopLevel(GameComp, allActionCreators);
+export const App = DragDropContext(HTML5Backend)(connectTopLevel(GameComp, allActionCreators));
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,
